b098227543fc370931fbd08848244cf4bf2d7492
artontech <artontechnology@gmail.com>
2023-03-28 01:00:18 +0800

    update requirements

--- install/install.bat
+++ install/install.bat
@@ -1,3 +1,6 @@
 @ECHO OFF
-pip install -r requirements.txt
+SET MIRROR=-i https://pypi.tuna.tsinghua.edu.cn/simple/
+cd /d %~dp0
+pip install --upgrade pip wheel
+pip install -r requirements.txt %MIRROR%
 pause
